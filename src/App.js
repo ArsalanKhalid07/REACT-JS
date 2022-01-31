1051d5f1d7d8f62e40ec316e37d90eb849ecf9bf
arsalan <arsalan.khalid@hyperdatacomputing.com>
2022-01-31 14:42:21 +0500

    About useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  //use state hook
-  //first value is intial value and second value is setter value
   const [count, setcount] = useState(0);
+  const [valX, setvalX] = useState(0);
+  const [valY, setvalY] = useState(0);
+  const logMouse = (e) => {
+    setvalX(e.clientX);
+    setvalY(e.clientY);
+  };
+
+  //NOTED
+  // useEffect hook
+  // first intial useeffect  as a component didmount
+  //add dependency of array as a second argument and add depend dependence when the componet is run . which is work as a component did updated
+  //clean up the component  using return function . which work as a componet  will un mount
+  //the count in the useeffect array . when count updated then useeffect runs
+
+  useEffect(() => {
+    window.addEventListener('mousemove', logMouse);
+    console.log(valX, valY);
+
+    return () => {
+      window.removeEventListener('mousemove', logMouse);
+      console.log('component will unmount');
+    };
+  }, [count]);
+
   return (
     <div className="App">
       <button
@@ -15,6 +37,9 @@ function App() {
       >
         {count}
       </button>
+      <p>X axis: {valX}</p>
+
+      <p>Y axis: {valY}</p>
     </div>
   );
 }
