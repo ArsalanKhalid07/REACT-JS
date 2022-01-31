714de5583017e9deb6ca9c06bad83cb926f2d917
arsalan <arsalan.khalid@hyperdatacomputing.com>
2022-01-31 12:17:56 +0500

    usestate hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //use state hook
+  //first value is intial value and second value is setter value
+  const [count, setcount] = useState(0);
   return (
     <div className="App">
-      <h2>hooks in react js</h2>
+      <button
+        onClick={() => {
+          setcount(count + 1);
+        }}
+      >
+        {count}
+      </button>
     </div>
   );
 }
