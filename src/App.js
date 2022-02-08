348c7129a57e4ac000beffcd3a7f4ded2313e170
arsalan <arsalan.khalid@hyperdatacomputing.com>
2022-02-08 16:47:37 +0500

    use reducer hook video 1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import './App.css';
 import React from 'react';
-import ComponentA from './Components/ComponentA';
+import Counter from './hooks/Counter';
 
-//USE CONTEXT HOOK
-// data pass from parent to nested child through props is cases lacking in application  so thats why use useContext hook.
-// props drilling avoiding
-
-//firts case : create context and provide value to main componet and use these value as consumner ::: example component C
-//second case : create context and prvide value to main componet and use usecontext hook to hnadle these provider value ::: example component B
-
-export const userContext = React.createContext();
-export const channelContext = React.createContext();
+//USEREDUCER HOOK
+//use reducer hook take 2 parameter one:[reducer ] and two:[intail value]
+//reducer take 2 parameter  one:[state] and two:[action]
 
 function App() {
-  const box = {
-    name: 'arsalan',
-    class: 'bscs',
-  };
-  const channel = {
-    name: 'channel',
-    class: 'accounts',
-  };
   return (
     <div className="App">
-      <userContext.Provider value={box}>
-        <channelContext.Provider value={channel}>
-          <ComponentA />
-        </channelContext.Provider>
-      </userContext.Provider>
+      <h2>use reducer hook</h2>
+      <Counter />
     </div>
   );
 }
