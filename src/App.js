167883ef4b2f660c2889b53a82d5df2f5df11995
arsalan <arsalan.khalid@hyperdatacomputing.com>
2022-02-09 12:16:45 +0500

    use reducer complex state and action {object use and multiple intial value}

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
-import Counter from './hooks/Counter';
+// import Counter from './hooks/Counter';
+import CounterTwo from './hooks/CouterTwo';
 
 //USEREDUCER HOOK
 //use reducer hook take 2 parameter one:[reducer ] and two:[intail value]
@@ -10,7 +11,8 @@ function App() {
   return (
     <div className="App">
       <h2>use reducer hook</h2>
-      <Counter />
+      {/* <Counter /> */}
+      <CounterTwo />
     </div>
   );
 }
